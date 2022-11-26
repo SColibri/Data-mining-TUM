597b73c2ed7789048337821e08486bb896a0b4b6
SColibri <sebastian.tonatiuh@gmail.com>
2022-11-26 17:07:35 +0000

    add java prime numbers

--- Task_3/Java/prime/src/main/java/com/prime/App.java
+++ Task_3/Java/prime/src/main/java/com/prime/App.java
@@ -1,10 +1,21 @@
 package com.prime;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.Console;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -15,18 +26,43 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Progressable;
 /**
- * Hello world!
- *
+ * Original code can be found online, sadly I forgot to paste the reference. The code
+ * was adapted to our setup.
  */
 public class App 
 {
+	// we ignore input in this class, but you can add the input manually if you want
     public static void main(String[] args) throws Exception {
+		// -----------------------------------------------
+		// 					FILE PATHS
+		// -----------------------------------------------
+		String primeNumbersSource = "/home/ubuntu/resources/primeNumbers.txt";
+		String hadoopPrimeSource = "/home/ubuntu/input/primeNumbers.txt";
+		String hadoopPrimeOutput = "/home/ubuntu/out/primeNumbers.out";
+		String hadoopAddress = "hdfs://192.168.4.83:9000";
+
+		// -----------------------------------------------
+		// 				CREATE NUMBER LIST
+		// -----------------------------------------------
+		
+		// if file does not exist, we create one, this takes 
+		// some time, so eat something or watch a movie
+		create_primeNumberList(primeNumbersSource);
+
+		// -----------------------------------------------
+		// 					JOB SETUP
+		// -----------------------------------------------
+
+		// create new hadoop configuration object
 		Configuration conf = new Configuration();
 
+		// create new hadoop mapreduce job
 		Job job = new Job(conf, "Primes");
 		job.setJarByClass(App.class);
 
+		// 
 		job.setOutputKeyClass(NullWritable.class);
 		job.setOutputValueClass(IntWritable.class);
 
@@ -35,24 +71,42 @@ public class App
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 
-		FileInputFormat.addInputPath(job, new Path("/home/ubuntu/input/filein.txt"));
-		FileOutputFormat.setOutputPath(job, new Path("/home/ubuntu/out/fileout.txt"));
+		FileInputFormat.addInputPath(job, new Path(hadoopPrimeSource));
+		FileOutputFormat.setOutputPath(job, new Path(hadoopPrimeOutput));
+
+		// copy input file into hadoop filesystem
+		check_ifInputExists(hadoopAddress, 
+							primeNumbersSource, 
+							hadoopPrimeOutput, 
+							hadoopPrimeSource, 
+							conf);
 
 		job.waitForCompletion(true);
 	}
 
+	// Extend the Mapper class
 	public static final class PrimesMap extends Mapper<LongWritable, Text, NullWritable, IntWritable> {
 		final NullWritable nw = NullWritable.get();
 
 		public final void map(final LongWritable key, final Text value, final Context context)
 				throws IOException, InterruptedException {
-			final int number = Integer.parseInt(value.toString());
-			if(isPrime(number)) {
-				context.write(nw, new IntWritable(number));
+			if (value == null) return;
+			if (value.getLength() == 0) return;
+			
+			try {
+				final int number = Integer.parseInt(value.toString());
+				if(isPrime(number)) {
+					context.write(nw, new IntWritable(number));
+				}
+			} catch (Exception e) {
+				// just ignore this entry
+				return;
 			}
+			
 		}
 	}
 
+	// Check if number is prime
 	private static final boolean isPrime(final int number) {
 		if (number == 1) {
 			return false;
@@ -71,5 +125,80 @@ public class App
 		}
 		System.console().writer().println(number);
 		return true;
+	}
+
+	/* 
+	 * Creates a file that contains 1E9 numbers
+	*/
+	private static void create_primeNumberList(String filename)
+	{
+		try {
+			
+			File fileObject = new File(filename);
+			if(fileObject.exists()) return;
+
+			BufferedWriter bufwriter = new BufferedWriter(new FileWriter(filename));
+			String toWriter;
+			
+
+			int bufferSize = 500;
+			for (int i = 0; i < 1E9; i += bufferSize) 
+			{
+				toWriter = "";
+				
+				// We write to hadoop filesystem after each
+				// interval, since memory access is costly
+				// but we can do better using char[] instead
+				// of string.
+				for (int j = 0; j < bufferSize; j++) 
+				{
+					toWriter += ( i + j ) + "\n";
+				}
+				
+				bufwriter.write(toWriter);
+			}
+
+			bufwriter.close();//closes the file
+		} catch (Exception e) {
+			// do nothing
+			return;
+		}
+	}
+
+	private static void check_ifInputExists(String hadoopAddress, String primeNumbersSource, String hadoopPrimeOutput, String hadoopPrimeSource, Configuration conf)
+	{
+		try {
+			// local filename
+			InputStream inputStream = new BufferedInputStream(new FileInputStream(primeNumbersSource));
+			
+			//hdfs instance
+			FileSystem hdfs = FileSystem.get(new URI(hadoopAddress), conf);
+			
+			// Delete previous folder bc hadoop does not like to overwrite stuff?
+			Path output = new Path(hadoopAddress + hadoopPrimeOutput);
+			if (hdfs.exists(output)) {
+				hdfs.delete(output, true);
+			}
+
+			//copy the input file into hadoop ~9.3GB
+			OutputStream outputStream = hdfs.create(new Path(hadoopAddress + hadoopPrimeSource),
+			new Progressable() {  
+					@Override
+					public void progress() {
+				System.out.println("....");
+					}
+							});
+			try
+			{
+				IOUtils.copyBytes(inputStream, outputStream, 4096, false); 
+			}
+			finally
+			{
+				IOUtils.closeStream(inputStream);
+				IOUtils.closeStream(outputStream);
+			} 
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 }
